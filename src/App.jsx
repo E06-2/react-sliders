c593467dc64bbd30bfd7b7ca78cd75e6b90c5f9b
jsk900 <jsk900@yahoo.co.uk>
2021-12-01 16:00:23 +0100

    sliders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import Header from './components/Header';
 import './App.css';
 
 const App = () => {
+  const [colour, setColour] = useState({ red: '00', green: '00', blue: '00' });
+
+  const sliderChange = (e) => {
+    setColour({ ...colour, [e.target.name]: e.target.value });
+  };
+
   return (
-    <main>
-     <h1>Welcome to React</h1>
-    </main>
+    <>
+      <Header colour={colour} sliderChange={sliderChange} />
+      <main
+        style={{
+          backgroundColor: `rgb(${colour.red}, ${colour.green}, ${colour.blue})`,
+        }}></main>
+    </>
   );
-}
+};
 
 export default App;
-
