185628d0557cc8b7842385b3eb31610f5ff6d4de
jsk900 <jsk900@yahoo.co.uk>
2022-02-25 11:19:31 +0100

    E08 React Slider RGB Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import './App.css';
 
 const App = () => {
-  const [colour, setColour] = useState({ red: '00', green: '00', blue: '00' });
+  const [colour, setColour] = useState({ red: '0', green: '0', blue: '0' });
 
   const sliderChange = (e) => {
     setColour({ ...colour, [e.target.name]: e.target.value });
